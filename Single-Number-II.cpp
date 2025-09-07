a0d03f9c5871794d4d7b033a36ce1d293cb26aa2
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2025-09-07 13:14:34 +0530

    [137] [Time Beats: 100.00%] [Memory Beats: 38.63%] - LeetPush

--- Single-Number-II.cpp
+++ Single-Number-II.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        int ans=-1;
-        sort(begin(nums),end(nums));
-        for(int i=1;i<nums.size();i+=3){
-            if(nums[i]==nums[i-1]&&nums[i]==nums[i+1]){
-                continue;
-            }
-                if(nums[i]!=nums[i-1]){
-                    ans=nums[i-1];
-                    break;
-                }
-                
+        int ones=0;
+        int twos=0;
+        for(int i=0;i<n;i++){
+            ones=(ones^nums[i])&~twos;
+            twos=(twos^nums[i])&~ones;
         }
-        if(ans==-1)return nums[n-1];
-        return ans;
+        return ones;
     }
 };
