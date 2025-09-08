38849de0858390482bc7f0078b74bd4ead478d66
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2025-09-08 23:34:47 +0530

    [1021] [Time Beats: 100.00%] [Memory Beats: 23.85%] - LeetPush

--- Remove-Outermost-Parentheses.cpp
+++ Remove-Outermost-Parentheses.cpp
@@ -3,26 +3,34 @@ public:
     string removeOuterParentheses(string s) {
         int n=s.length();
         int count=0;
-        vector<string>a;
-        int start=-1;
+        // vector<string>a;
+        // int start=-1;
+        // for(int i=0;i<n;i++){
+        //    if(s[i]=='('){
+        //     count++;
+        //    if(start==-1) start=i;
+
+        //     }
+        //    if(s[i]==')')count--;
+
+        //    if(count==0){
+        //     a.push_back(s.substr(start+1,i-start-1));
+        //     start=-1;
+        //    }
+        // }
+
+        // string ans="";
+        // for(auto it:a){
+        //     ans+=it;
+        // }
+        string a="";
         for(int i=0;i<n;i++){
-           if(s[i]=='('){
-            count++;
-           if(start==-1) start=i;
-
+            if(s[i]==')'){
+                count--;
             }
-           if(s[i]==')')count--;
-
-           if(count==0){
-            a.push_back(s.substr(start+1,i-start-1));
-            start=-1;
-           }
+            if(count!=0)a+=s[i];
+            if(s[i]=='(')count++;
         }
-
-        string ans="";
-        for(auto it:a){
-            ans+=it;
-        }
-        return ans;
+        return a;
     }
 };
