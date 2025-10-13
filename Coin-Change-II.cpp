71996a8da6e98a6d1d5ac12824358a1d3e1e289f
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2025-10-13 23:46:09 +0530

    [518] [Time Beats: 10.42%] [Memory Beats: 5.97%] - LeetPush

--- Coin-Change-II.cpp
+++ Coin-Change-II.cpp
@@ -1,23 +1,40 @@
+int mod=1e10+7;
 class Solution {
 public:
-int solve(int i,int tar,vector<int>&coins, vector<vector<int>>&dp){
-   if(i==0){
-    if(tar%coins[0]==0){
-        return 1;
-    }
-    return 0;
-   }
-   if(dp[i][tar]!=-1)return dp[i][tar];
-   int nt=solve(i-1,tar,coins,dp);
-   int t=0;
-   if(coins[i]<=tar){
-    t=solve(i,tar-coins[i],coins,dp);
-   }
-   return dp[i][tar]=nt+t;
-}
+// int solve(int i,int tar,vector<int>&coins, vector<vector<int>>&dp){
+//    if(i==0){
+//     if(tar%coins[0]==0){
+//         return 1;
+//     }
+//     return 0;
+//    }
+
+//    if(dp[i][tar]!=-1)return dp[i][tar];
+//    int nt=solve(i-1,tar,coins,dp);
+//    int t=0;
+//    if(coins[i]<=tar){
+//     t=solve(i,tar-coins[i],coins,dp);
+//    }
+//    return dp[i][tar]=nt+t;
+// }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);
+        vector<vector<long long>>dp(n+1,vector<long long>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=amount;tar++){
+                      long long nt=dp[i-1][tar];
+                       long long t=0;
+                       if(coins[i]<=tar){
+                        t=dp[i][tar-coins[i]];
+                       }
+                       dp[i][tar]=(nt+t)%mod;
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
