b1e82b5ebf0c6a8d842db49b1b2026dcec320726
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2025-10-29 14:28:02 +0530

    [1716] [Time Beats: 100.00%] [Memory Beats: 56.58%] - LeetPush

--- Calculate-Money-in-Leetcode-Bank.cpp
+++ Calculate-Money-in-Leetcode-Bank.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int startMoney=1;
-        int res=0;
-       while(n>0){
-        int money=startMoney;
-        for(int i=1;i<=min(n,7);i++){
-               res+=money;
-               money++;
-        }
-        n=n-7;
-        startMoney++;
+    //     int startMoney=1;
+    //     int res=0;
+    //    while(n>0){
+    //     int money=startMoney;
+    //     for(int i=1;i<=min(n,7);i++){
+    //            res+=money;
+    //            money++;
+    //     }
+    //     n=n-7;
+    //     startMoney++;
 
-       }
-       return res;
+    //    }
+    //    return res;
+
+    
+    int terms=n/7;
+    int f=28;
+    int l=28+(terms-1)*7;
+
+    int res=terms*(f+l)/2;
+
+
+    int start=1+terms;
+
+   for(int day=1;day<=(n%7);day++){
+    res+=start;
+    start++;
+   }
+   return res;
+
+
+
+
+
+
+
+
+
+
+
     }
 };
