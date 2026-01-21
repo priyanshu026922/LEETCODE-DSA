d95fa2277a5f8e09a99b6e64d1b7323ab9e9e6a3
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-01-21 11:25:57 +0530

    [3315] [Time Beats: 100.00%] [Memory Beats: 83.50%] - LeetPush

--- DCP-01-26/Construct-the-Minimum-Bitwise-Array-II.cpp
+++ DCP-01-26/Construct-the-Minimum-Bitwise-Array-II.cpp
@@ -6,18 +6,24 @@
 6         for(int j=0;j<n;j++){
 7            if(nums[j]==2)continue;
 8              int val=nums[j];
-9              int mini=val;
+9              bool found=true;
 10            for(int i=0;i<32;i++){
 11                if(val&(1<<i)){
-12                    int x=(val&(~(1<<i)));
-13                    if((x|(x+1))==nums[j]){
-14                              mini=min(mini,x);
-15                    }
-16                }
-17            }
-18            ans[j]=mini;
-19
+12                    continue;
+13                }
+14                int prev=i-1;
+15                int x=(val^(1<<prev));
+16                ans[j]=x;
+17                break;
+18            }
+19        
 20         }
 21         return ans;
 22    }
 23};
+24
+25
+26// int x=(val&(~(1<<i)));
+27//                     if((x|(x+1))==nums[j]){
+28//                               mini=min(mini,x);
+29//                     }
