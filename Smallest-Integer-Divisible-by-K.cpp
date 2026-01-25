ee852b405ab4253bf9396ceb6607c69ae0fdb664
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-01-25 13:02:33 +0530

    [1015] [Time Beats: 31.70%] [Memory Beats: 79.87%] - LeetPush

--- Smallest-Integer-Divisible-by-K.cpp
+++ Smallest-Integer-Divisible-by-K.cpp
@@ -3,7 +3,7 @@
 3    int smallestRepunitDivByK(int k) {
 4       long i=0;
 5       long long rem=0;
-6       while(i<1e5){
+6       while(i<k){
 7           rem=(10*rem+1)%k;
 8           if(rem==0)return i+1;
 9           i++;
