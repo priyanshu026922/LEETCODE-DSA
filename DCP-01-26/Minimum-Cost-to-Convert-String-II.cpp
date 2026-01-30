47d3d9ae03266515d82bdfa7c36f294d440f6824
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-01-30 20:39:37 +0530

    [2977] [Time Beats: 55.17%] [Memory Beats: 32.18%] - LeetPush

--- DCP-01-26/Minimum-Cost-to-Convert-String-II.cpp
+++ DCP-01-26/Minimum-Cost-to-Convert-String-II.cpp
@@ -7,76 +7,92 @@
 7    string sourceStr;
 8    string targetStr;
 9    set<int>validLen;
-10    ll dk(string src,string tar){
-11        priority_queue<pair<ll,string>,vector<pair<ll,string>>,greater<pair<ll,string>>>pq;
-12        unordered_map<string,ll>res;
-13        res[src]=0;
-14        pq.push({0,src});
-15        while(!pq.empty()){
-16            ll currCost=pq.top().first;
-17            string node=pq.top().second;
-18            pq.pop();
-19            if(node==tar){
-20                break;
-21            }
-22
-23            for(auto &edge:adj[node]){
-24                string adjNode=edge.first;
-25                ll edgeCost=edge.second;
-26                if(!res.count(adjNode)||currCost+edgeCost<res[adjNode]){
-27                    res[adjNode]=currCost+edgeCost;
-28                    pq.push({currCost+edgeCost,adjNode});
-29                }
-30            }
-31        }
-32
-33        ll final=res.count(tar)?res[tar]:val;
-34        return final;
-35    }
-36    ll solve(int idx){
-37        if(idx>=sourceStr.length()){
-38            return 0;
+10
+11    unordered_map<string,unordered_map<string,ll>>dkMemo;
+12
+13    ll dk(string src,string tar){
+14
+15        if(dkMemo[src].count(tar)){
+16            return dkMemo[src][tar];
+17        }
+18
+19        priority_queue<pair<ll,string>,vector<pair<ll,string>>,greater<pair<ll,string>>>pq;
+20        unordered_map<string,ll>res;
+21        res[src]=0;
+22        pq.push({0,src});
+23        while(!pq.empty()){
+24            ll currCost=pq.top().first;
+25            string node=pq.top().second;
+26            pq.pop();
+27            if(node==tar){
+28                break;
+29            }
+30
+31            for(auto &edge:adj[node]){
+32                string adjNode=edge.first;
+33                ll edgeCost=edge.second;
+34                if(!res.count(adjNode)||currCost+edgeCost<res[adjNode]){
+35                    res[adjNode]=currCost+edgeCost;
+36                    pq.push({currCost+edgeCost,adjNode});
+37                }
+38            }
 39        }
 40
-41        if(dpMemo[idx]!=-1)return dpMemo[idx];
-42
-43        ll miniCost=val;
-44        if(sourceStr[idx]==targetStr[idx]){
-45            miniCost=solve(idx+1);
-46        }
-47
-48        for(auto &len:validLen){
-49            if(idx+len>sourceStr.length()){
-50                break;
-51            }
-52            string src=sourceStr.substr(idx,len);
-53            string tar=targetStr.substr(idx,len);
-54
-55            if(!adj.count(src)){
-56                continue;
-57            }
-58            ll minPath=dk(src,tar);
-59            if(minPath==1e10)continue;
-60
-61            miniCost=min(miniCost,minPath+solve(idx+len));
-62        }
-63        return dpMemo[idx]=miniCost;
-64    }
+41        ll final=res.count(tar)?res[tar]:val;
+42        return dkMemo[src][tar]=final;
+43    }
+44
+45
+46
+47    ll solve(int idx){
+48        if(idx>=sourceStr.length()){
+49            return 0;
+50        }
+51
+52        if(dpMemo[idx]!=-1)return dpMemo[idx];
+53
+54        ll miniCost=val;
+55        if(sourceStr[idx]==targetStr[idx]){//IF MATCHED ,WE HAVE A OPTION TO FIND POSSIBLE ANS IN ANY NEXT STEP ,SKIPPING THIS STEP!!!!!!
+56            miniCost=solve(idx+1);
+57        }
+58
+59        for(auto &len:validLen){
+60            if(idx+len>sourceStr.length()){
+61                break;
+62            }
+63            string src=sourceStr.substr(idx,len);
+64            string tar=targetStr.substr(idx,len);
 65
-66    ll minimumCost(string source, string target, vector<string>& original, vector<string>&changed, vector<int>& cost) {
-67          sourceStr=source;
-68          targetStr=target;
+66            if(!adj.count(src)){
+67                continue;
+68            }
 69
-70          dpMemo.assign(10001,-1);
+70            ll minPath=dk(src,tar);
 71
-72          for(int i=0;i<original.size();i++){
-73            adj[original[i]].push_back({changed[i],cost[i]});
-74          }
+72
+73
+74            if(minPath==1e10)continue;
 75
-76          for(auto &it:original){
-77            validLen.insert(it.length());
-78          }
-79          ll res=solve(0);
-80          return res==val?-1:res;
-81    }
-82};
+76            miniCost=min(miniCost,minPath+solve(idx+len));
+77        }
+78        return dpMemo[idx]=miniCost;
+79    }
+80
+81    ll minimumCost(string source, string target, vector<string>& original, vector<string>&changed, vector<int>& cost) {
+82          sourceStr=source;
+83          targetStr=target;
+84
+85          dpMemo.assign(10001,-1);//FOR MEMOIZING THE SOLUTION
+86
+87          for(int i=0;i<original.size();i++){
+88               adj[original[i]].push_back({changed[i],cost[i]});//ADJACENY MATRIX
+89          }
+90
+91          for(auto &it:original){ //to get possible length of original array string
+92            validLen.insert(it.length());
+93          }
+94
+95          ll res=solve(0);
+96          return res==val?-1:res;
+97    } 
+98};
