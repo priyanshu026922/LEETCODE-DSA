f717f9d806e54175c77cd74d5f88fa5af503b9a8
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-02-15 13:07:22 +0530

    [67] [Time Beats: 100.00%] [Memory Beats: 30.97%] - LeetPush

---
+++ DCP-02-26/Add-Binary.cpp
@@ -0,0 +1,63 @@
+1class Solution {
+2public:
+3    string addBinary(string a, string b) {
+4        //LET SUPPOSE a.length()>b.length()
+5        if(b.length()>a.length()){
+6            swap(a,b);
+7        }
+8        int n=a.length();
+9        int m=b.length();
+10        int val=n-m;
+11        string s="";
+12        for(int i=0;i<val;i++){
+13               s+='0';
+14        }
+15        for(int i=0;i<m;i++){
+16            s+=b[i];
+17        }
+18        cout<<a<<":::: "<<s<<endl;
+19         
+20        string ans=a;
+21        int carry=0;
+22        for(int i=n-1;i>=0;i--){
+23            if(a[i]=='1'&&s[i]=='1'){
+24               if(carry==1){
+25                  ans[i]='1';
+26               }else{
+27                ans[i]='0';
+28               }
+29                carry=1;
+30            }else if(a[i]=='1'&&s[i]=='0'){
+31                if(carry==1){
+32                  ans[i]='0';
+33                  carry=1;
+34                }else{
+35                  ans[i]='1';
+36                  carry=0;
+37                }
+38            }else if(a[i]=='0'&&s[i]=='1'){
+39                if(carry==1){
+40                  ans[i]='0';
+41                  carry=1;
+42                }else{
+43                  ans[i]='1';
+44                  carry=0;
+45                }
+46            }else{
+47                 if(carry==1){
+48                  ans[i]='1';
+49                }else{
+50                  ans[i]='0';
+51                }
+52                 carry=0;
+53            }
+54        }
+55        
+56        if(carry==1){
+57            reverse(begin(ans),end(ans));
+58            ans+='1';
+59             reverse(begin(ans),end(ans));
+60        }
+61        return ans;
+62    }
+63};
