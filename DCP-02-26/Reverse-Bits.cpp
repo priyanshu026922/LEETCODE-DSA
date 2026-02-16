b02b92ab4315dba01b1a9864171426b2933f8d32
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-02-16 11:44:46 +0530

    [190] [Time Beats: 100.00%] [Memory Beats: 9.32%] - LeetPush

--- DCP-02-26/Reverse-Bits.cpp
+++ DCP-02-26/Reverse-Bits.cpp
@@ -3,7 +3,7 @@
 3    int reverseBits(int n) {
 4        vector<int>bits(32,0);
 5        for(int i=0;i<32;i++){
-6            if((n&(1<<i))){
+6            if(n&(1<<i)){
 7                  bits[i]=1;
 8            }
 9        }
