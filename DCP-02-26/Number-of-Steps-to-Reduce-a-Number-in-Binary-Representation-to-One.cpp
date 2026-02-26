93ffccb82f9a6668e5938fe7d8286c3b0cf35aec
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-02-26 17:52:48 +0530

    [1404] [Time Beats: 100.00%] [Memory Beats: 58.55%] - LeetPush

--- DCP-02-26/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One.cpp
+++ DCP-02-26/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One.cpp
@@ -1,36 +1,17 @@
 1class Solution {
 2public:
-3void getString(string &s){//FOR ODD Case
-4    int n=s.length();
-5    bool b=false;
-6    int i=n-1;
-7    while(i>=0&&s[i]!='0'){
-8        s[i]='0';
-9        i--;
-10    }
-11    
-12    if(i<0){
-13        s="1"+s;
-14        return;
-15    }
-16        s[i]='1';
-17   
-18}
-19
-20    int numSteps(string s) {
-21        int n=s.length();
-22        int cnt=0;
-23        int last=n-1;
-24        while(s.length()>1){
-25            int m=s.length();
-26            if(s[m-1]=='1'){//ODD
-27               getString(s);
-28            }else{//EVEN
-29            //    getLeft(s);
-30               s=s.substr(0,m-1);
-31            }
-32            cnt++;
-33        }
-34        return cnt;
-35    }
-36};
+3    int numSteps(string s) {
+4        int n=s.length();
+5        int op=0;
+6        int carry=0;
+7        for(int i=n-1;i>=1;i--){
+8            if(((s[i]-'0')+carry)%2==1){
+9                op+=2;
+10                carry=1;
+11            }else{
+12                op++;
+13            }
+14        }
+15        return op+carry;
+16    }
+17};
