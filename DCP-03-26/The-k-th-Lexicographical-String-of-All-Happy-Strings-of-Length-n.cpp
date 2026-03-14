b55ae530cfd35982f62c4c9d64d31f10783aea61
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-03-14 11:37:17 +0530

    [1415] [Time Beats: 47.08%] [Memory Beats: 64.49%] - LeetPush

--- DCP-03-26/The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
+++ DCP-03-26/The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
@@ -1,32 +1,27 @@
 1class Solution {
 2public:
-3    void solve(string s,char last,int n, vector<string>&ans){
+3    void solve(string s,char last,int n, string &res,int &cnt,int k){
 4        if(s.length()==n){
-5            ans.push_back(s);
-6            return;
-7        }
-8        if(last!='a'){
-9            solve(s+'a','a',n,ans);
+5            cnt++;
+6            if(cnt==k){
+7                res=s;
+8            }
+9            return;
 10        }
-11        if(last!='b'){
-12            solve(s+'b','b',n,ans);
-13        }
-14         if(last!='c'){
-15            solve(s+'c','c',n,ans);
-16        }
-17
-18    }
-19    string getHappyString(int n, int k){
-20        vector<string>ans;
-21        solve("",'~',n,ans);
-22        if(ans.size()<k){
-23            return "";
-24        }
-25        sort(begin(ans),end(ans));
-26        // for(int i=0;i<ans.size();i++){
-27        //     cout<<ans[i]<<" ";
-28        // }
-29        // cout<<endl;
-30        return ans[k-1];
-31    }
-32};
+11
+12        for(char ch='a';ch<='c';ch++){
+13            if(ch!=last){
+14                s+=ch;;
+15                solve(s,ch,n,res,cnt,k);
+16                s.pop_back();
+17            }
+18        }
+19    }
+20
+21    string getHappyString(int n, int k){
+22        string res="";
+23        int cnt=0;
+24        solve("",'~',n,res,cnt,k);
+25        return res;
+26    }
+27};
