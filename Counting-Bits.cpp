ca8e138dd3399bdd62a464a832484c33fff00abc
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-03-17 22:13:35 +0530

    [338] [Time Beats: 38.41%] [Memory Beats: 58.20%] - LeetPush

--- Counting-Bits.cpp
+++ Counting-Bits.cpp
@@ -1,19 +1,14 @@
-class Solution {
-public:
-int countSet(int n){
-    int count=0;
-    while(n!=0){
-       n=n&(n-1);
-    count++;
-    }
-    return count;
-}
-    vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0;i<=n;i++){
-            int x=countSet(i);
-            ans.push_back(x);
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> countBits(int n) {
+4        vector<int>dp(n+1,0);
+5         int s=1;
+6        for(int i=1;i<=n;i++){
+7          if(s*2==i){
+8            s=i;
+9          }
+10          dp[i]=dp[i-s]+1;
+11        }
+12        return dp;
+13    }
+14};
