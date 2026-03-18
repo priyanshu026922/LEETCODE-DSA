d9fcb5ca847063d909d442db38d52839dc3c7bab
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-03-18 10:03:39 +0530

    [3070] [Time Beats: 82.02%] [Memory Beats: 74.72%] - LeetPush

--- DCP-03-26/Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-k.cpp
+++ DCP-03-26/Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-k.cpp
@@ -3,21 +3,15 @@
 3    int countSubmatrices(vector<vector<int>>& grid, int k) {
 4        int n=grid.size();
 5        int m=grid[0].size();
-6        
-7        vector<vector<int>>pref(n,vector<int>(m,0));
-8        for(int i=0;i<n;i++){
-9            for(int j=0;j<m;j++){
-10              pref[i][j]=grid[i][j]+(j>0?pref[i][j-1]:0)+(i>0?pref[i-1][j]:0)-(i>0&&j>0?pref[i-1][j-1]:0);
-11            }
-12        }
-13        int cnt=0;
-14        for(int i=0;i<n;i++){
-15            for(int j=0;j<m;j++){
-16                if(pref[i][j]<=k){
-17                    cnt++;
-18                }
-19            }
-20        }
-21        return cnt;
-22    }
-23};
+6        int cnt=0;
+7        for(int i=0;i<n;i++){
+8            for(int j=0;j<m;j++){
+9              grid[i][j]=grid[i][j]+(j>0?grid[i][j-1]:0)+(i>0?grid[i-1][j]:0)-(i>0&&j>0?grid[i-1][j-1]:0);
+10              if(grid[i][j]<=k){
+11                    cnt++;
+12                }
+13            }
+14        } 
+15        return cnt;
+16    }
+17};
