b456270004c7b0caa131026d3dc830430d3dfdff
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-03-30 13:11:17 +0530

    [2840] [Time Beats: 64.94%] [Memory Beats: 84.48%] - LeetPush

--- DCP-03-26/Check-if-Strings-Can-be-Made-Equal-With-Operations-II.cpp
+++ DCP-03-26/Check-if-Strings-Can-be-Made-Equal-With-Operations-II.cpp
@@ -2,36 +2,23 @@
 2public:
 3    bool checkStrings(string s1, string s2) {
 4        int n=s1.length();
-5        unordered_map<char,int>e;
-6        unordered_map<char,int>o;
+5       vector<int>e(26,0);
+6       vector<int>o(26,0);
 7
 8        for(int i=0;i<n;i++){
 9            if(i%2==0){
-10                e[s1[i]]++;
-11            }else{
-12                o[s1[i]]++;
-13            }
-14        }
-15
-16        for(int i=0;i<n;i++){
-17            if(i%2==0){
-18               if(e.find(s2[i])==e.end()){
-19                return false;
-20               }
-21               e[s2[i]]--;
-22               if(e[s2[i]]==0){
-23                e.erase(s2[i]);
-24               }
-25            }else{
-26                if(o.find(s2[i])==o.end()){
-27                  return false;
-28                }
-29               o[s2[i]]--;
-30               if(o[s2[i]]==0){
-31                o.erase(s2[i]);
-32               }
-33            }
-34        }
-35        return true;
-36    }
-37};
+10                e[s1[i]-'a']++;
+11                e[s2[i]-'a']--;
+12            }else{
+13                o[s1[i]-'a']++;
+14                o[s2[i]-'a']--;
+15            }
+16        }
+17
+18        for(int i=0;i<26;i++){
+19           if(e[i]!=0||o[i]!=0)return false;
+20        }
+21
+22        return true;
+23    }
+24};
