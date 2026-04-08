4014fd905de8be630639837f1462a6d9246cf86c
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-04-08 10:08:15 +0530

    [2069] [Time Beats: 72.56%] [Memory Beats: 7.32%] - LeetPush

--- Walking-Robot-Simulation-II.cpp
+++ Walking-Robot-Simulation-II.cpp
@@ -1,45 +1,45 @@
 1class Robot {
 2public:
-3    int n,m;
-4    int ind=0;
-5    bool move=false;
-6    vector<vector<int>>grid;
-7    Robot(int width, int height) {
-8        // grid.assign(width,vector<int>(height));
-9        n=width;
-10        m=height;
-11        //bottom row(l->r):EAST
-12        for(int i=0;i<width;i++){
-13            grid.push_back({i,0,0});
-14        }
-15        //right row(d-->t):NORTH(1)
-16        for(int j=1;j<height;j++){
-17            grid.push_back({width-1,j,1});
-18        }
-19        //top row(r-->l):WEST(2)
-20        for(int i=width-2;i>=0;i--){
-21              grid.push_back({i,height-1,2});
-22        }
-23        //left row(t-->b):SOUTH(3)
-24        for(int j=height-2;j>0;j--){
-25            grid.push_back({0,j,3});
-26        }
-27        grid[0][2]=3;//make it south for idx =0(CORNER CASE)
-28    }
-29    
-30    void step(int num) {
-31       move=true;
-32       ind=(ind+num)%grid.size();
-33    }
-34    
-35    vector<int> getPos() {
-36       return {grid[ind][0],grid[ind][1]};
-37    }
-38    
-39    string getDir(){
-40        if(!move){
-41            return "East";
-42        }
+3    int ind=0;
+4    bool move=false;
+5    vector<vector<int>>grid;
+6    Robot(int width, int height) {
+7        // grid.assign(width,vector<int>(height));
+8        
+9        //bottom row(l->r):EAST
+10        for(int i=0;i<width;i++){
+11            grid.push_back({i,0,0});
+12        }
+13        //right row(d-->t):NORTH(1)
+14        for(int j=1;j<height;j++){
+15            grid.push_back({width-1,j,1});
+16        }
+17        //top row(r-->l):WEST(2)
+18        for(int i=width-2;i>=0;i--){
+19              grid.push_back({i,height-1,2});
+20        }
+21        //left row(t-->b):SOUTH(3)
+22        for(int j=height-2;j>0;j--){
+23            grid.push_back({0,j,3});
+24        }
+25        grid[0][2]=3;//make it south for idx=0(CORNER CASE)
+26        //while retruning to {0,0}-->it goes back in SOUTH dir
+27    }
+28    
+29    void step(int num) {
+30       move=true;
+31       ind=(ind+num)%grid.size();
+32    }
+33    
+34    vector<int> getPos() {
+35       return {grid[ind][0],grid[ind][1]};
+36    }
+37    
+38    string getDir(){
+39        if(!move){
+40            return "East";
+41        }
+42
 43        int dir=grid[ind][2];
 44        if(dir==0){
 45           return "East";
