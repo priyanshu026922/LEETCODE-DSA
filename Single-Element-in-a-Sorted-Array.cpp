14efb441f0b21949c981ccf4213809537d8e4e42
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-05-10 23:00:08 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 6.13%] - LeetPush

--- Single-Element-in-a-Sorted-Array.cpp
+++ Single-Element-in-a-Sorted-Array.cpp
@@ -1,10 +1,29 @@
 1class Solution {
 2public:
 3    int singleNonDuplicate(vector<int>& nums) {
-4        int x=0;
-5        for(int i=0;i<nums.size();i++){
-6            x=x^nums[i];
-7        }
-8        return x;
-9    }
-10};
+4       int n=nums.size();
+5       if(n==1)return nums[0];
+6
+7       if(nums[0]!=nums[1])return nums[0];
+8       if(nums[n-1]!=nums[n-2])return nums[n-1];
+9
+10       int l=1;
+11       int r=n-2;
+12       while(l<=r){
+13           int mid=l+(r-l)/2;
+14
+15           if((nums[mid]!=nums[mid-1])&&nums[mid]!=nums[mid+1]){
+16                 return nums[mid];
+17           }
+18           
+19           //EOEOEO.....
+20           if(((mid%2==1)&&(nums[mid-1]==nums[mid]))||((mid%2==0)&&(nums[mid+1]==nums[mid]))){
+21               l=mid+1;
+22           }else{
+23               r=mid-1;
+24           }
+25
+26       }
+27       return -1;
+28    }
+29};
