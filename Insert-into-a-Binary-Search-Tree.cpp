8cc45955043d141ce95701736ca049c1b8812be0
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-05-29 12:20:44 +0530

    [701] [Time Beats: 100.00%] [Memory Beats: 23.78%] - LeetPush

--- Insert-into-a-Binary-Search-Tree.cpp
+++ Insert-into-a-Binary-Search-Tree.cpp
@@ -1,41 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root==NULL){
-            return new TreeNode(val);
-        }
-
-        TreeNode*curr=root;
-        while(curr){
-            if(curr->val<=val){
-                if(curr->right){
-                    curr=curr->right;
-                }else{
-                    curr->right=new TreeNode(val);
-                    break;
-                }
-            }else{
-                if(curr->left){
-                    curr=curr->left;
-                }else{
-                    curr->left=new TreeNode(val);
-                    break;
-                }
-            }
-        }
-        
-
-       return root;
-        }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* insertIntoBST(TreeNode* root, int val) {
+15        if(!root){
+16            return new TreeNode(val);
+17        }
+18
+19        TreeNode* temp=root;
+20        while(temp){
+21            if(val>temp->val){
+22                if(temp->right){
+23                    temp=temp->right;
+24                }else{
+25                    temp->right=new TreeNode(val);
+26                    break; 
+27                }
+28            }else{
+29                 if(temp->left){
+30                    temp=temp->left;
+31                }else{
+32                    temp->left=new TreeNode(val);
+33                    break; 
+34                }
+35            }
+36        }
+37        return root;
+38    }
+39};
