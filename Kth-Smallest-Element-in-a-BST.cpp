e7eab4e40f1c5376cdeaba1c1b1f52baf1820d40
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-05-29 14:18:32 +0530

    [230] [Time Beats: 8.18%] [Memory Beats: 89.19%] - LeetPush

--- Kth-Smallest-Element-in-a-BST.cpp
+++ Kth-Smallest-Element-in-a-BST.cpp
@@ -1,48 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int kthSmallest(TreeNode* root, int k) {
-        TreeNode*curr=root;
-        int res=-1;
-        int count=0;
-        while(curr){
-             if(!curr->left){
-               count++;
-               if(count==k){
-                res=curr->val;
-               }
-               curr=curr->right;
-             }else{
-                   TreeNode*prev=curr->left;
-                   while(prev->right&&prev->right!=curr)
-             {
-                prev=prev->right;
-             }
-             if(!prev->right){
-                prev->right=curr;
-                curr=curr->left;
-             }else{
-                prev->right=NULL;
-                count++;
-               if(count==k){
-                res=curr->val;
-               }
-                curr=curr->right;
-             
-             }
-        }
-       
-    }
-     return res;
-    }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution { 
+13public:
+14    int kthSmallest(TreeNode* root, int k) {
+15        
+16        TreeNode*curr=root;
+17        int cnt=0;
+18        int ans=-1;
+19        while(curr){
+20            if(!curr->left){
+21                 cnt++;
+22                 if(cnt==k){
+23                    ans=curr->val;
+24                 }
+25                curr=curr->right;
+26            }else{//curr->left exists
+27                TreeNode*prev=curr->left;
+28                while(prev->right&&prev->right!=curr){
+29                    prev=prev->right;
+30                }
+31
+32                if(!prev->right){
+33                    prev->right=curr;
+34                    curr=curr->left;
+35                }
+36
+37                else {//prev->right NOT NULL
+38                     prev->right=NULL;
+39                     cnt++;
+40                     if(cnt==k){
+41                        ans= curr->val;
+42                     }
+43                      
+44                     curr=curr->right;
+45                    
+46                }
+47
+48            }
+49
+50           
+51        }
+52        return ans;
+53    }
+54};
