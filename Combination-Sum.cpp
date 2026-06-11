a53dbf8d12fae3323223b6cf51b8d69b80d42cc3
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-06-11 13:01:10 +0530

    [39] [Time Beats: 51.62%] [Memory Beats: 54.19%] - LeetPush

--- Combination-Sum.cpp
+++ Combination-Sum.cpp
@@ -2,21 +2,21 @@
 2public:
 3int n;
 4    void solve(vector<int>&c,int i,int target,vector<int>&a,  vector<vector<int>>&ans){
-5
-6        if(target==0){
-7            ans.push_back(a);
-8            return;
-9        }
-10       if(i==n){
+5        if(target==0){
+6            ans.push_back(a);
+7            return;
+8        }
+9
+10        if(i==n){
 11          if(target==0){
 12            ans.push_back(a);
 13          }
 14          return;
-15       }
+15        }
 16
-17       if(target<0){
-18    return;
-19       }
+17        if(target<0){
+18           return;
+19        }
 20
 21
 22        if(target>=c[i]){
@@ -28,16 +28,15 @@
 28            a.pop_back();
 29        }
 30
-31        solve(c,i+1,target,a,ans);
+31       if(i==0||(i>0&&c[i]!=c[i-1])) solve(c,i+1,target,a,ans);
 32
-33
-34    }
-35
-36    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-37        vector<vector<int>>ans;
-38        vector<int>a;
-39        n=candidates.size();
-40        solve(candidates,0,target,a,ans);
-41        return ans;
-42    }
-43};
+33    }
+34
+35    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+36        vector<vector<int>>ans;
+37        vector<int>a;
+38        n=candidates.size();
+39        solve(candidates,0,target,a,ans);
+40        return ans;
+41    }
+42};
