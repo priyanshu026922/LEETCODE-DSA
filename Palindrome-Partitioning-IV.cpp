b97e46fa75762017a02c5c7948288c55c39e60c2
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-06-16 15:29:42 +0530

    [1745] [Time Beats: 18.37%] [Memory Beats: 5.51%] - LeetPush

--- Palindrome-Partitioning-IV.cpp
+++ Palindrome-Partitioning-IV.cpp
@@ -2,62 +2,50 @@
 2private:
 3    int n;
 4public:
-5    void cal(string &s,vector<vector<int>>&pal){
-6
-7        for(int len=1;len<=n;len++){
-8                for(int i=0;i<n+1-len;i++){
-9                    int j=len+i-1;
-10
-11                    if(s[i]==s[j]){
-12                        if(len<=2){
-13                            pal[i][j]=1;
-14                        }else{
-15                            pal[i][j]=pal[i+1][j-1];
-16                        }
-17                    }
-18                }
-19            }
-20    }
-21
-22
-23//    bool isPalindrome(int i,int j,vector<vector<int>>&pal){
-24//        return pal[i][j];
-25//    }
-26
-27    // bool solve(string &s,int i,int cnt,vector<vector<int>>&pal,  vector<vector<int>>&dp){
-28    //     if(i==s.length()){
-29    //         return (cnt==3);
-30    //     }
-31
-32    //     if(dp[i][cnt]!=-1)return dp[i][cnt];
+5   bool isPalindrome(int i,int j,vector<vector<int>>&pal){
+6       return pal[i][j];
+7   }
+8    bool solve(string &s,int i,int cnt,vector<vector<int>>&pal,  vector<vector<int>>&dp){
+9        if(cnt>3)return 0;
+10        
+11        if(i==s.length()){
+12            return (cnt==3);
+13        }
+14
+15        if(dp[i][cnt]!=-1)return dp[i][cnt];
+16
+17        for(int j=i;j<n;j++){
+18            if(isPalindrome(i,j,pal)){
+19                if(solve(s,j+1,cnt+1,pal,dp)){
+20                    return dp[i][cnt]=true;
+21                }
+22            }
+23        }
+24
+25        return dp[i][cnt]=false;
+26    }
+27
+28    bool checkPartitioning(string s) {
+29        n=s.length();
+30        vector<vector<int>>pal(n,vector<int>(n,0));
+31          vector<vector<int>>dp(n,vector<int>(n,-1));
+32        
 33
-34    //     for(int j=i;j<n;j++){
-35    //         if(isPalindrome(i,j,pal)){
-36    //             if(solve(s,j+1,cnt+1,pal,dp)){
-37    //                 return dp[i][cnt]=true;
-38    //             }
-39    //         }
-40    //     }
-41
-42    //     return dp[i][cnt]=false;
-43    // }
-44
-45    bool checkPartitioning(string s) {
-46        n=s.length();
-47        vector<vector<int>>p(n,vector<int>(n,0));
-48        cal(s,p);
-49       
-50
-51       for(int i=0;i<n;i++){
-52         for(int j=n-1;j>i+1;j--){
-53
-54            if(p[0][i]&&p[j][n-1]&&p[i+1][j-1]){
-55                return true;
-56            }
-57         }
-58       }
-59
-60
-61        return false;
-62    }
-63};
+34        for(int len=1;len<=n;len++){
+35            for(int i=0;i<n+1-len;i++){
+36                int j=len+i-1;
+37
+38                if(s[i]==s[j]){
+39                    if(len<=2){
+40                        pal[i][j]=1;
+41                    }else{
+42                        pal[i][j]=pal[i+1][j-1];
+43                    }
+44                }
+45            }
+46        }
+47
+48
+49        return solve(s,0,0,pal,dp);
+50    }
+51};
