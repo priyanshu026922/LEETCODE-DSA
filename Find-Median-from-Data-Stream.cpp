a06b12b86bb621a51f0d06270ac0d335debd3012
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-06-23 22:20:05 +0530

    [295] [Time Beats: 59.49%] [Memory Beats: 63.67%] - LeetPush

--- Find-Median-from-Data-Stream.cpp
+++ Find-Median-from-Data-Stream.cpp
@@ -1,49 +1,46 @@
 1class MedianFinder {
-2    priority_queue<int>pq1;
-3priority_queue<int,vector<int>,greater<int>>pq2;
-4public:
-5
-6    MedianFinder(){} 
-7
-8
-9    
-10    void addNum(int num) {
-11
-12        int n=pq1.size();
-13        int m=pq2.size();
-14        if(pq1.empty()||num<pq1.top()){
-15            pq1.push(num);
-16        }else{
-17          pq2.push(num); 
-18        } 
-19
-20
-21        if(pq2.size()>pq1.size()){
-22             pq1.push(pq2.top());
-23             pq2.pop();
-24        }else if(pq1.size()>pq2.size()+1){
-25            pq2.push(pq1.top());
-26            pq1.pop();
+2public:
+3priority_queue<int>pq1;
+4priority_queue<int,vector<int>,greater<int>>pq2;
+5    MedianFinder() {
+6        
+7    }
+8    
+9    void addNum(int num) {
+10        if(pq1.empty()||num<=pq1.top()){
+11            pq1.push(num);
+12        }else{
+13            pq2.push(num);
+14        }
+15
+16        //hmesha x+1 and x sieze maintian kreke chleenge
+17        if(pq1.size()>1+pq2.size()){
+18            int x=pq1.top();
+19            pq1.pop();
+20            pq2.push(x);
+21        }
+22
+23        if(pq2.size()>pq1.size()){
+24             int x=pq2.top();
+25            pq2.pop();
+26            pq1.push(x);
 27        }
-28
-29    }
-30    
-31    double findMedian() {
-32        int n=pq1.size();
-33        int m=pq2.size();
-34        double val=-1;
-35        if((n+m)%2!=0){
-36           return pq1.top();
-37        }
-38
-39       val=((double)pq1.top()+(double)pq2.top())/2.0;
-40        return val;
-41    }
-42};
-43
-44/**
-45 * Your MedianFinder object will be instantiated and called as such:
-46 * MedianFinder* obj = new MedianFinder();
-47 * obj->addNum(num);
-48 * double param_2 = obj->findMedian();
-49 */
+28    }
+29    
+30    double findMedian() {
+31        int n=pq1.size();
+32        int m=pq2.size();
+33        if((n+m)%2==0){
+34            double ans=(double)pq1.top()+(double)pq2.top();
+35            return ans/2.0;
+36        }
+37        return (double)pq1.top();
+38    }
+39};
+40
+41/**
+42 * Your MedianFinder object will be instantiated and called as such:
+43 * MedianFinder* obj = new MedianFinder();
+44 * obj->addNum(num);
+45 * double param_2 = obj->findMedian();
+46 */
