09f59acb60f6904b1bbe5f3a59b697e81c01f390
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-06-25 16:38:41 +0530

    [44] [Time Beats: 40.34%] [Memory Beats: 40.40%] - LeetPush

--- Wildcard-Matching.cpp
+++ Wildcard-Matching.cpp
@@ -1,48 +1,43 @@
 1class Solution {
-2    private:
-3    int n,m;
-4    int dp[2001][2001];
-5public:
-6    bool solve(string &s, string &p,int i,int j){
-7         if(i<0&&j<0){
-8            return true;
-9         }
-10
-11         if(j<0){  //means i>=0
-12             return false;
-13         }
+2public:
+3    bool isMatch(string s, string p) {
+4       int  n=s.length();
+5       int m=p.length();
+6        
+7
+8        vector<int>prev(m+1,0);
+9        // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+10        prev[0]=1;
+11              int k=1;
+12              while(k<=m){
+13                if(p[k-1]!='*')break;
 14
-15         if(i<0){
-16            while(j>=0&&p[j]=='*'){
-17                j--;
-18            }
-19            return j<0;
-20         }
-21         
-22         if(dp[i][j]!=-1)return dp[i][j];
-23
-24        if(s[i]==p[j]){
-25            return  dp[i][j]=solve(s,p,i-1,j-1);
-26        }
-27
-28        if(p[j]=='?'){
-29            return  dp[i][j]=solve(s,p,i-1,j-1);
-30        }
+15                prev[k]=1;
+16                k++;
+17              }
+18
+19
+20              for(int i=1;i<=n;i++){
+21                vector<int>curr(m+1,0);
+22                for(int j=1;j<=m;j++){
+23                     if(s[i-1]==p[j-1]){
+24                         curr[j]=prev[j-1];
+25                    }
+26
+27                    else if(p[j-1]=='?'){
+28                        curr[j]=prev[j-1];
+29                    }
+30
 31
-32
-33        if(p[j]=='*'){
-34            return dp[i][j]=solve(s,p,i-1,j)||solve(s,p,i,j-1);
-35        }
-36
-37        return dp[i][j]=false;
+32                    else if(p[j-1]=='*'){
+33                        curr[j]=prev[j]||curr[j-1];
+34                    }
+35
+36                    else curr[j]=false;
+37             }
 38
-39    }
-40    bool isMatch(string s, string p) {
-41        n=s.length();
-42        m=p.length();
-43        
-44        memset(dp,-1,sizeof(dp));
-45
-46        return solve(s,p,n-1,m-1);
-47    }
-48};
+39             prev=curr;
+40              }
+41               return prev[m];
+42    }
+43};
