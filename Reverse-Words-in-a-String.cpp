53f3ae40b2de3b0df7aa48be90515157332fea07
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-06-29 21:22:44 +0530

    [151] [Time Beats: 100.00%] [Memory Beats: 95.34%] - LeetPush

--- Reverse-Words-in-a-String.cpp
+++ Reverse-Words-in-a-String.cpp
@@ -1,27 +1,35 @@
-class Solution {
-public:
-    string reverseWords(string s) {
-        stack<string>st;
-        int n=s.length();
-        string a="";
-        for(int i=0;i<n;i++){
-            if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='0'&&s[i]<='9')){
-                a+=s[i];
-            }else{
-                 if(a!="")       st.push(a);
-                        a="";
-            }
-        }
-        if(a!=""){
-            st.push(a);
-        }
-        string w="";
-        while(!st.empty()){
-            string u=st.top();
-            st.pop();
-            w+=u;
-            if(!st.empty())w+=' ';
-        }
-        return w;
-    }
-};
+1class Solution {
+2public:
+3    string reverseWords(string s) {
+4        int n=s.length();
+5        int l=0;
+6        int r=0;
+7        int i=0;
+8        
+9        reverse(begin(s),end(s));
+10        while(i<n){
+11           
+12           while(i<n&&s[i]==' '){
+13             i++;
+14           }
+15
+16           if(i==n)break;
+17
+18          while(i<n&&s[i]!=' '){
+19            s[r]=s[i];
+20            r++;
+21            i++;
+22          }
+23
+24
+25          reverse(begin(s)+l,begin(s)+r);
+26
+27          s[r]=' ';
+28          r++;
+29          l=r;
+30          i++;
+31        }
+32        s.resize(r-1);
+33        return s;
+34    }
+35};
