864d3309e753d8df6ffa85a255dab5145025a485
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-07-01 21:53:38 +0530

    [424] [Time Beats: 48.92%] [Memory Beats: 41.21%] - LeetPush

--- Longest-Repeating-Character-Replacement.cpp
+++ Longest-Repeating-Character-Replacement.cpp
@@ -1,29 +1,34 @@
-class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        int i=0;
-        int j=0;
-        int n=s.length();
-        int maxi_freq=0;
-        int maxlen=0;
-        unordered_map<char,int>mp;
-        while(j<n){
-              mp[s[j]]++;
-              maxi_freq=max(maxi_freq,mp[s[j]]);
-
-             if((j-i+1)-maxi_freq>k){
-                    mp[s[i]]--;
-                    for(auto it:mp){
-                        maxi_freq=max(maxi_freq,it.second);
-                    }
-                    i++;
-               }
-
-               if((j-i+1)-maxi_freq<=k){
-                 maxlen=max(maxlen,j-i+1);
-                }
-                j++;
-        }
-        return maxlen;
-    }
-};
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        int i=0;
+5        int j=0;
+6        int n=s.length();
+7        int m=0;
+8        vector<int>vec(26,0);
+9        int maxi=0;
+10        while(j<n){
+11           vec[s[j]-'A']++;
+12           
+13           
+14           for(int k=0;k<26;k++){
+15               maxi=max(maxi,vec[k]);
+16           }
+17           
+18           int len=j-i+1;
+19           while(i<n&&len-maxi>k){
+20              vec[s[i]-'A']--;
+21              for(int k=0;k<26;k++){
+22                 maxi=max(maxi,vec[k]);
+23              }
+24              i++;
+25              len=j-i+1;
+26           }
+27
+28
+29           m=max(m,j-i+1);
+30           j++;
+31        }
+32        return m;
+33    }
+34};
