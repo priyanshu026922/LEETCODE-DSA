bdb93039967a8a2b6b71d8607e3b344cebfc12c6
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-07-01 12:41:55 +0530

    [1781] [Time Beats: 39.62%] [Memory Beats: 31.50%] - LeetPush

--- Sum-of-Beauty-of-All-Substrings.cpp
+++ Sum-of-Beauty-of-All-Substrings.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
-    int beautySum(string s) {
-        int n=s.length();
-      unordered_map<char,int>mp;
-       int sum=0;
-        for(int i=0;i<n;i++){
-            mp.clear();
-            for(int j=i;j<n;j++){
-                mp[s[j]]++;
-                int maxi=INT_MIN;
-            int mini=INT_MAX;
-            for(auto it:mp){
-                maxi=max(maxi,it.second);
-                 mini=min(mini,it.second);
-            }
-                sum+=(maxi-mini);
-            }
-        }
-        return sum;
-    }
-};
+1class Solution {
+2public:
+3    int beautySum(string s) {
+4        int n=s.length();
+5      
+6       int sum=0;
+7        for(int i=0;i<n;i++){
+8            unordered_map<char,int>mp;
+9            for(int j=i;j<n;j++){
+10                mp[s[j]]++;
+11                int maxi=INT_MIN;
+12            int mini=INT_MAX;
+13            for(auto it:mp){
+14                maxi=max(maxi,it.second);
+15                 mini=min(mini,it.second);
+16            }
+17                sum+=(maxi-mini);
+18            }
+19        }
+20        return sum;
+21    }
+22};
