f959329212e84a34bc081caf4eeb9f85fd6b0589
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-08-21 15:11:01 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 60.85%] - LeetPush

---
+++ 152-Maximum-Product-Subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int prod=1;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            
+            if(prod==0)prod=1;
+            prod*=nums[i];
+            maxi=max(maxi,prod);
+        }
+        
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            
+            if(prod==0)prod=1;
+            prod*=nums[i];
+            maxi=max(maxi,prod);
+        }
+       
+        return maxi;
+    }
+};
