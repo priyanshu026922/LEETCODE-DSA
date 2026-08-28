5db8dc818230cad60eb7b983bc8db086a841a601
Priyanshu Ranjan <btech10886.23@bitmesra.ac.in>
2026-08-28 12:30:58 +0530

    [44] [Time Beats: 5.01%] [Memory Beats: 47.81%] - LeetPush

--- 44-Wildcard-Matching.cpp
+++ 44-Wildcard-Matching.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
+int dp[2001][2001];
     int n,m;
-    int dp[2001][2001];
     bool solve(string &s,string &p,int i,int j){
-       if(j<0){
-           return i<0;   
-       }
+        //i----->for string s [with only lowercase characters]
 
-       if(i<0){
+        //j----->for string p [with lowercase characters,*,?]
+        if(i<0&&j<0)return true;
+        if(j<0){
+            return false;
+        }
+        if(i<0){
             for(int k=0;k<=j;k++){
                 if(p[k]!='*')return false;
             }
             return true;
-       }
-
-       if(dp[i][j]!=-1)return dp[i][j];
-
-        if(s[i]==p[j]){
-            return dp[i][j]=solve(s,p,i-1,j-1);
         }
 
-        if(p[j]=='?'){
-            return dp[i][j]=solve(s,p,i-1,j-1);
-        }
+        
+        if(dp[i][j]!=-1)return dp[i][j];
 
-        if(p[j]=='*'){
+
+        if(s[i]==p[j]||p[j]=='?'){
+            return dp[i][j]= solve(s,p,i-1,j-1);
+        }
+         if(p[j]=='*'){
             return dp[i][j]=solve(s,p,i-1,j)|solve(s,p,i,j-1);
         }
-
+         
         return dp[i][j]=false;
+
     }
     bool isMatch(string s, string p) {
         n=s.length();
         m=p.length();
-       memset(dp,-1,sizeof(dp));
+        memset(dp,-1,sizeof(dp));
         return solve(s,p,n-1,m-1);
     }
 };
